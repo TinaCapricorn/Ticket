2946f6289bf6ad15c981147e2027e89eebf33985
Klein Kristina <klein281293@gmail.com>
2022-02-16 18:25:22 +0700

    Fix2

--- src/test/java/ru/netology/ManagerTest.java
+++ src/test/java/ru/netology/ManagerTest.java
@@ -49,9 +49,21 @@ class ManagerTest {
     @Test
     void saveTicket() {
         manager.addTicket(new Ticket(11, 448, "PEE", "LED", 48));
-        int expected = 11;
-        int actual = manager.getTickets().length;
-        assertEquals(expected, actual);
+        Ticket[] expected = {
+                new Ticket(1, 100500, "ABA", "BZK", 150),
+                new Ticket(2, 400, "NOZ", "VVO", 52),
+                new Ticket(3, 10600, "KMW", "KYZ", 235),
+                new Ticket(4, 3501, "MQF", "NYM", 628),
+                new Ticket(5, 15901, "GOJ", "ABA", 43),
+                new Ticket(6, 12589, "KGD", "GOJ", 648),
+                new Ticket(7, 110, "ODS", "PEE", 245),
+                new Ticket(8, 666, "LED", "SLY", 344),
+                new Ticket(9, 985, "BZK", "MQF", 578),
+                new Ticket(10, 10, "NOZ", "VVO", 247),
+                new Ticket(11, 448, "PEE", "LED", 48)
+        };
+        Ticket[] actual = manager.getTickets();
+        assertArrayEquals(expected, actual);
     }
 
     @Test
